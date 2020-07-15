9d7fd4fce86bb2beaef060c72ad752f559d8b415
raphaelMi <raphael.m024@gmail.com>
2020-07-15 19:24:51 +0200

    no longer compute distances for every k

--- main.py
+++ main.py
@@ -19,11 +19,9 @@ def R(data1, data2):
 
 # return list of indices of nearest points
 # x is in array-shape
-def k_nearest(data, x, k):  # very time consuming (~2.6 sec)
-    cen = np.repeat(data[:, 1:][:, np.newaxis, :], len(x), axis=1) - x  # (~0.6 sec)
-    sq = np.square(cen)  # (~0.2 sec)
-    dist = np.sum(sq, axis=2)  # (~0.4 sec)
-    return np.argsort(dist, axis=0)[:k]  # (~1.4 sec)
+def k_nearest(data, x):  # very time consuming
+    dist = sp.distance.cdist(data[:, 1:], x, 'sqeuclidean')  # (~0.1 sec)
+    return np.argsort(dist, axis=0)  # (~1.4 sec)
 
 
 def k_nearest_alt(data, x, k):
@@ -31,8 +29,21 @@ def k_nearest_alt(data, x, k):
     return np.argpartition(dist, k, axis=0)[:k]  # (~0.5 sec)
 
 
+# computes f_D,k for given x value for k in array shape
+def f_naive(data, x, kset):
+    near = k_nearest(data, x)  # using k_nearest to only compute it once
+    y = data[:, :1]
+    nearest_bin = np.take_along_axis(np.repeat(y, len(x), axis=1), near, axis=0)
+    results = []
+    for k in kset:
+        result = np.sign(np.sum(nearest_bin[:k], axis=0))
+        result[result == 0] = 1  # sets sign(0) to 1
+        results.append(result)
+    return results
+
+
 # computes f_D,k for given x values
-def f_naive(data, x, k):
+def f_naive_alt(data, x, k):
     near = k_nearest_alt(data, x, k)  # using k_nearest_alt because it is faster
     y = data[:, :1]
     nearest_bin = np.take_along_axis(np.repeat(y, len(x), axis=1), near, axis=0)
@@ -47,7 +58,7 @@ def f_final(data_segmented, x, k):
 
     for i, di in enumerate(data_segmented):
         di_complement = np.concatenate(np.delete(data_segmented, i, axis=0))
-        tmp = tmp + f_naive(di_complement, x, k)
+        tmp = tmp + f_naive_alt(di_complement, x, k)
 
     result = np.sign(tmp)
     result[result == 0] = 1  # sets sign(0) to 1
@@ -72,18 +83,15 @@ def classify(name, kset=K, l=5, output=True):
     # this way we have d_i = dd[i]
     dd = np.array_split(train, l)
 
-    k_best_r = np.ones((len(kset), l))
+    k_best_r = np.ones((l, len(kset)))
 
-    for n, k in enumerate(kset):
-        if output:
-            print("testing k =", k)
+    for i, di in enumerate(dd):
+        di_complement = np.concatenate(np.delete(dd, i, axis=0))
 
-        for i, di in enumerate(dd):
-            di_complement = np.concatenate(np.delete(dd, i, axis=0))
-            r = R(di, stitch(f_naive(di_complement, di[:, 1:], k), di[:, 1:]))
-            k_best_r[n][i] = r
+        for n, f in enumerate(f_naive(di_complement, di[:, 1:], kset)):
+            k_best_r[i][n] = R(di, stitch(f, di[:, 1:]))
 
-    k_best = K[np.argmin(np.mean(k_best_r, axis=1))]
+    k_best = K[np.argmin(np.mean(k_best_r, axis=0))]
     print('k* =', k_best)
 
     test = dataset.parse('data/' + name + '.test.csv')
@@ -94,7 +102,7 @@ def classify(name, kset=K, l=5, output=True):
     compare = f_final(dd, test[:, 1:], k_best)
     print('Failure rate (compared to test data):', R(test, stitch(compare, test[:, 1:])))
     if output:
-        visual.display_2d_dataset(stitch(compare, test[:, 1:])) # Display guessed labels of test data
+        visual.display_2d_dataset(stitch(compare, test[:, 1:]))  # Display guessed labels of test data
 
     # grid = [[n/100, m/100] for n in range(100) for m in range(100)]
     # visual.display_2d_dataset(stitch(f_final(dd, grid, k_best), grid)) # Display grid
